add9dedaac137e115584640789a6d1e141717eed
tis025995 <suzuki.toyohiro@tis.co.jp>
2019-02-14 13:33:52 +0900

    20190214 new

--- src/test/java/com/nablarch/example/client/UpdateClient.java
+++ src/test/java/com/nablarch/example/client/UpdateClient.java
@@ -1,5 +1,130 @@
 package com.nablarch.example.client;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+
+import com.nablarch.example.dto.ProjectResponseDto;
+import com.nablarch.example.entity.Project;
+import com.nablarch.example.form.ProjectUpdateForm;
+
+import nablarch.core.beans.BeanUtil;
+import nablarch.core.beans.CopyOptions;
+
 public class UpdateClient {
 
+    private static final String targetUrl = "http://localhost:9080/projects";
+
+    /**
+     * プロジェクト情報の文字列変換を行う。
+     * @param args 引数
+     */
+    public static void main(String[] args) throws Exception {
+
+        // 更新対象プロジェクト取得
+        Map<String, String> updateTargetProject = getProjects("projectName", "プロジェクト９９９").get(0);
+        final CopyOptions copyOptions = CopyOptions.options()
+                                                   .datePatternByName("projectStartDate", "yyyy/MM/dd")
+                                                   .datePatternByName("projectEndDate", "yyyy/MM/dd")
+                                                   .build();
+
+        Project updateProject = BeanUtil.createAndCopy(Project.class, updateTargetProject, copyOptions);
+        ProjectUpdateForm updateForm = setUpdateProject(updateProject);
+
+        // 更新
+        System.out.println("update status:" + putProject(updateForm, updateProject.getProjectId()));
+        System.out.println(getProjects());
+    }
+
+
+    /**
+     * 更新用プロジェクト情報設定
+     * @param project 更新対象プロジェクト情報
+     * @return 更新用プロジェクト情報
+     */
+    private static ProjectUpdateForm setUpdateProject(Project project) {
+        ProjectUpdateForm form = new ProjectUpdateForm();
+        form.setProjectId(project.getProjectId().toString());
+        form.setProjectName("プロジェクト８８８");
+        form.setProjectType("development");
+        form.setProjectClass("a");
+        form.setProjectStartDate("20150101");
+        form.setProjectEndDate("20150331");
+        form.setClientId("1");
+        form.setProjectManager("佐藤");
+        form.setProjectLeader("鈴木");
+        form.setNote("備考８８８");
+        form.setSales("20000");
+        form.setCostOfGoodsSold("2000");
+        form.setSga("3000");
+        form.setAllocationOfCorpExpenses("4000");
+        return form;
+    }
+
+    /**
+     * HTTP GETメソッドを使用したクライアント操作を行う。
+     * @return プロジェクト情報リスト
+     */
+    private static List<Map<String, String>> getProjects() {
+        return ClientBuilder.newClient()
+                .target(targetUrl)
+                .request(MediaType.APPLICATION_JSON)
+                .get(new GenericType<List<Map<String, String>>>() {});
+    }
+
+    /**
+     * HTTP GETメソッドを使用したクライアント操作を行う。
+     * @param key query paramのキー
+     * @param value query paramの値
+     * @return プロジェクト情報リスト
+     */
+    private static List<Map<String, String>> getProjects(String key, Object value) throws UnsupportedEncodingException {
+
+        return ClientBuilder.newClient()
+                            .target(targetUrl)
+                            .queryParam(key, value)
+                            .request(MediaType.APPLICATION_JSON)
+                            .get(new GenericType<List<Map<String, String>>>() {});
+    }
+
+    /**
+     * HTTP PUTメソッドを使用したクライアント操作を行う。
+     * @param project 更新用プロジェクト情報
+     * @param projectId プロジェクトID
+     * @return ステータスコード
+     */
+    private static Integer putProject(ProjectUpdateForm project, Integer projectId) {
+        return ClientBuilder.newClient()
+                .target(targetUrl)
+                .request(MediaType.APPLICATION_JSON)
+                .put(Entity.json(project)).getStatus();
+    }
+
+    /**
+     * プロジェクト情報の文字列変換を行う。
+     * @param projects プロジェクト情報List
+     * @return プロジェクト情報
+     */
+    private static String makeDataString(List<ProjectResponseDto> projects) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("---- projects (size: %s) ----", projects.size())).append('\n');
+        for (ProjectResponseDto project : projects) {
+            sb.append(String.format("Project(ProjectId: %s, ProjectName: %s, ProjectType: %s, ProjectClass: %s, "
+                    + "ProjectStartDate: %s, ProjectEndDate: %s, ClientId: %s, ProjectManager: %s, ProjectLeader: %s, "
+                    + "UserId: %s, Note: %s, Sales: %s, CostOfGoodsSold: %s, Sga: %s, AllocationOfCorpExpenses: %s, "
+                    + "Client: %s, SystemAccount: %s)",
+                    project.getProjectId(), project.getProjectName(), project.getProjectType(),
+                    project.getProjectClass(), project.getProjectStartDate(), project.getProjectEndDate(),
+                    project.getClientId(), project.getProjectManager(), project.getProjectLeader(),
+                    project.getUserId(), project.getNote(), project.getSales(), project.getCostOfGoodsSold(),
+                    project.getSga(), project.getAllocationOfCorpExpenses(), project.getClient(),
+                    project.getSystemAccount())).append('\n');
+        }
+        return sb.toString();
+    }
 }
